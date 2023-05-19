9ac02dea476cd62ff92348fa7c9cc1198134acca
sudirboyy <sudirboyy@gmail.com>
2023-05-19 11:17:00 +0700

    3

--- SingleLinkedList_178/SingleLinkedList_178.cpp
+++ SingleLinkedList_178/SingleLinkedList_178.cpp
@@ -20,19 +20,27 @@ void addNote() {
     nodeBaru->noMhs = nim;
     nodeBaru->name = nama;
 
-}
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+    if (START == NULL || nim <= START->noMhs) {
+        if (START != NULL && nim == START->noMhs)
+        {
+            cout << " NIM sudah ada " << endl;
+            return;
+        }
+        Node* current = START;
+        Node* previous = START;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << " NIM sudah ada " << endl;
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+        nodeBaru->next = current;
+        nodeBaru->next = nodeBaru;
+    }
+
