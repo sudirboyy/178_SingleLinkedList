2ade03e8801fa11ae712c65410591a0a44682d0c
sudirboyy <sudirboyy@gmail.com>
2023-05-19 10:59:02 +0700

    1

--- SingleLinkedList_178/SingleLinkedList_178.cpp
+++ SingleLinkedList_178/SingleLinkedList_178.cpp
@@ -1,7 +1,12 @@
-// SingleLinkedList_178.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+struct Node
+{
+    int noMhs;
+    string name;
+    Node* next;
+};
 
 int main()
 {
