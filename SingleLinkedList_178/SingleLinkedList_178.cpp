2e5056f7488b118e1d9043120b6b73092ab3e334
sudirboyy <sudirboyy@gmail.com>
2023-05-19 13:08:18 +0700

    12

--- SingleLinkedList_178/SingleLinkedList_178.cpp
+++ SingleLinkedList_178/SingleLinkedList_178.cpp
@@ -132,4 +132,16 @@ void searchData() {
 
 int main() {
     int pilihan;
-    do
+    do 
+    {
+        try
+        {
+
+            cout << "1. Tambah Data" << endl;
+            cout << "2. Hapus Data" << endl;
+            cout << "3. Tampilkan Data" << endl;
+            cout << "4. Cari Data" << endl;
+            cout << "5. Keluar" << endl;
+            cout << "pilihan: ";
+            cin >> pilihan;
+            switch (pilihan)
